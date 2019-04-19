cadd2192c19ef45ef40f62f6b6bf3dc68e2c64ed
aruyamam <aruyamam@gmail.com>
2019-04-19 10:48:49 +0900

    Middleware & Route Handlder

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const router = require('./router');
 
 const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(express.json({ type: '*/*' }));
+
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3000;
