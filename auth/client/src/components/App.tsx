d3973825c3b862aaf7eb6f630a17037c293075d2
aruyamam <aruyamam@gmail.com>
2019-04-25 10:11:10 +0900

    Creating the Header

--- auth/client/src/components/App.tsx
+++ auth/client/src/components/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
 
 const App = () => {
-  return (
-    <div>
-      Hi, im the app
-    </div>
-  )
-}
+   return (
+      <div>
+         <Header />
+         Hi, im the app
+      </div>
+   );
+};
 
-export default App
+export default App;
