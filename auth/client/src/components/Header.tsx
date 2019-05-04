942dfc9709e0d770e285f97f901ef7fd45438cfa
aruyamam <aruyamam@gmail.com>
2019-05-04 10:33:13 +0900

    Updating the Header

--- auth/client/src/components/Header.tsx
+++ auth/client/src/components/Header.tsx
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { IAppState } from '../reducers';
+import './HeaderStyle.css';
 
-export class Header extends Component {
-   render() {
+interface IProps {
+   authenticated: string | null;
+}
+
+export class Header extends Component<IProps> {
+   public render() {
+      return (
+         <div className="header">
+            <Link to="/">Redux Auth</Link>
+            {this.renderLinks()}
+         </div>
+      );
+   }
+
+   private renderLinks() {
+      if (this.props.authenticated) {
+         return (
+            <div>
+               <Link to="/signout">Sign Out</Link>
+               <Link to="/feature">Feature</Link>
+            </div>
+         );
+      }
+
       return (
          <div>
-            <Link to="/">Redux Auth</Link>
             <Link to="/signup">Sign Up</Link>
             <Link to="/signin">Sign In</Link>
-            <Link to="/signout">Sign Out</Link>
-            <Link to="/feature">Feature</Link>
          </div>
       );
    }
 }
 
-export default Header;
+function mapStateToProps(state: IAppState) {
+   return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
